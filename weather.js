7cdd1d8344f5aebb546064132cc7f58813304279
mharvic <31197516+mharvic@users.noreply.github.com>
2025-04-14 15:26:52 -0600

    Updated weather.js
    - Line 6-8: Removed the unnecessary lines that was not needed.
    
    +6-12: Added weather icon, description and cancun script.

--- weather.js
+++ weather.js
@@ -3,11 +3,11 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=cancun&appid=02eb660016
 .then(data =>{
     console.log(data);
     console.log(data.main.temp);
-    temperature.textContent = "Temperature " + data.main.temp +"\u00b0C"; // adding celcius symbol //
-    feelslike.textContent = "Feels Like: " + data.main.feels_like +"\u00b0C";
-    description.textContent ="" + data.weather.description;
+    temperature.textContent = "Cancún: " + data.main.temp +"\u00b0C"; // adding celcius symbol //
+    // feelslike.textContent = "Feels Like: " + data.main.feels_like +"\u00b0C";
+    // description.textContent ="" + data.weather[0].description;
 
     //inserting the icon
 
-    icon.src = ("https://openweathermap.org/img/wn/") + data.weather[0].icon + "@2x.png";
+    weathericon.src = ("https://openweathermap.org/img/wn/") + data.weather[0].icon + "@2x.png";
 }); 
